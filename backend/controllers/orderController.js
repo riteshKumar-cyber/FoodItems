f1f34239fa15980cf4fd30670bcf197d7611b9bb
ritesh kumar <kumarritesh86495@gmail.com>
2025-08-31 16:57:08 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ import foodModel from "../models/foodModel.js"; // ✅ make sure this is importe
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-const frontend_url = "http://localhost:5173";
+const frontend_url = "https://food-admin-oti7.onrender.com";
 
 const placeOrder = async (req, res) => {
   try {
