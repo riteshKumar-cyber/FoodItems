09da9d2c1a86585f4ed3173900bc82d18526c17d
ritesh kumar <kumarritesh86495@gmail.com>
2025-08-31 17:46:14 +0530

    Update StoreContext.jsx

--- food/src/context/StoreContext.jsx
+++ food/src/context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider =(props) =>{
    
 const[cartItems,setCartItems] = useState({});
-const url = "https://fooditems-4.onrender.com";
+const url = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+
 const [token,setToken] = useState("");
 const  [food_list,setFoodList] = useState([])
 
