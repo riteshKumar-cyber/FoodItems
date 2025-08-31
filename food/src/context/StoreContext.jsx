e97b78f7db050aa8f0e5fe3f81853abdf3c088ac
ritesh kumar <kumarritesh86495@gmail.com>
2025-08-31 19:01:43 +0530

    Update StoreContext.jsx

--- food/src/context/StoreContext.jsx
+++ food/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider =(props) =>{
    
 const[cartItems,setCartItems] = useState({});
-const url = import.meta.env.VITE_API_BASE_URL || "https://food-fronted-zeqy.onrender.com";
+const url = "https://food-fronted-zeqy.onrender.com";
 
 const [token,setToken] = useState("");
 const  [food_list,setFoodList] = useState([])
